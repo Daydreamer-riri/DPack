b2c92e0ca5e1cd5b1b8c6587bf0eacc3b1e976ed
ding.zhao <ding.zhao@trip.com>
2023-02-03 15:14:50 +0800

    feat: 新增res send方法

---
+++ packages/dpack/src/node/server/send.ts
@@ -0,0 +1,57 @@
+import type {
+  IncomingMessage,
+  OutgoingHttpHeaders,
+  ServerResponse,
+} from 'node:http'
+import getEtag from 'etag'
+
+const alias: Record<string, string | undefined> = {
+  js: 'application/javascript',
+  css: 'text/css',
+  html: 'text/html',
+  json: 'application/json',
+}
+
+export interface SendOptions {
+  etag?: string
+  cacheControl?: string
+  headers?: OutgoingHttpHeaders
+  map?: null
+}
+
+export function send(
+  req: IncomingMessage,
+  res: ServerResponse,
+  content: string | Buffer,
+  type: string,
+  options: SendOptions,
+) {
+  const {
+    etag = getEtag(content, { weak: true }),
+    cacheControl = 'no-cache',
+    headers,
+    map,
+  } = options
+
+  if (res.writableEnded) return
+
+  if (req.headers['if-none-match'] === etag) {
+    res.statusCode = 304
+    res.end()
+    return
+  }
+
+  res.setHeader('Content-Type', alias[type] || type)
+  res.setHeader('Cache-Control', cacheControl)
+  res.setHeader('Etag', etag)
+
+  if (headers) {
+    for (const name in headers) {
+      res.setHeader(name, headers[name]!)
+    }
+  }
+
+  res.statusCode = 200
+  res.end(content)
+  return
+}
